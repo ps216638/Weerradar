0b8559e86b39c44c0f97fbc533729ed2e0de10a8
ps216638 <ps216638@edu.summacollege.nl>
2023-05-16 15:08:57 +0200

    project

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueGtag from "vue-gtag";
 import './styles/app.css'
 import './registerServiceWorker'
 
-createApp(App).use(VueAxios, axios).mount('#app')
+createApp(App).use(VueGtag, {
+    config: { id: "G-ZZCQLKNR3W" }
+}).use(VueAxios, axios).mount('#app')
